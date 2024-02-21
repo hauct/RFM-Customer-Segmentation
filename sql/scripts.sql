c75ac7599092fe71ccf0316f93224aad5e2db958
hauct <cungtronghau@gmail.com>
2024-02-21 18:24:41 +0700

    edit something

--- sql/scripts.sql
+++ sql/scripts.sql
@@ -8,27 +8,34 @@ WITH rfm_cal AS(
 	where customer_id <> 0 
 	GROUP BY customer_id
 	HAVING sum(gmv) <> 0),
-rfm_score AS (
-SELECT *,
-CASE 
-	WHEN recency >= '92' THEN '1'
-	WHEN recency BETWEEN '62' AND '91' THEN '2'
-	WHEN recency BETWEEN '31' AND '61' THEN '3'
-	WHEN recency BETWEEN '1' AND '30' THEN '4'
-END AS r,
-CASE 
-	WHEN frequency <= '2' THEN '1'
-	WHEN frequency = '3' THEN '2'
-	WHEN frequency = '4' THEN '3'
-	WHEN frequency >= '5' THEN '4'
-END AS f,
-CASE 
-	WHEN monetary <= '210000' THEN '1'
-	WHEN monetary BETWEEN '210000' AND '266693' THEN '2'
-	WHEN monetary BETWEEN '266694' AND '300000' THEN '3'
-	WHEN monetary >= '300000' THEN '4'
-END AS m
-FROM rfm_cal
+-- rfm_score AS (
+-- SELECT *,
+-- CASE 
+-- 	WHEN recency >= '92' THEN '1'
+-- 	WHEN recency BETWEEN '62' AND '91' THEN '2'
+-- 	WHEN recency BETWEEN '31' AND '61' THEN '3'
+-- 	WHEN recency BETWEEN '1' AND '30' THEN '4'
+-- END AS r,
+-- CASE 
+-- 	WHEN frequency <= '2' THEN '1'
+-- 	WHEN frequency = '3' THEN '2'
+-- 	WHEN frequency = '4' THEN '3'
+-- 	WHEN frequency >= '5' THEN '4'
+-- END AS f,
+-- CASE 
+-- 	WHEN monetary <= '210000' THEN '1'
+-- 	WHEN monetary BETWEEN '210000' AND '266693' THEN '2'
+-- 	WHEN monetary BETWEEN '266694' AND '300000' THEN '3'
+-- 	WHEN monetary >= '300000' THEN '4'
+-- END AS m
+-- FROM rfm_cal
+-- ),
+rfm_score as (
+select *,
+NTILE(4) OVER (ORDER BY recency desc) as r,
+NTILE(4) OVER (ORDER BY frequency asc) as f,
+NTILE(4) OVER (ORDER BY monetary asc) as m
+from rfm_cal
 ), 
 rfm_all AS(
 SELECT *, concat(r,f,m) AS rfm FROM RFM_score),
@@ -43,11 +50,15 @@ CASE
 	WHEN rfm IN ('131', '132', '141', '142', '231', '232', '241', '242') THEN 'Price Sensitive'
 	WHEN rfm IN ('244', '234', '243', '233', '224', '214', '213', '134', '144', '143', '133') THEN 'Needs Attention'
 	WHEN rfm IN ('223', '221', '222', '211', '212', '124') THEN 'About to sleep'
-	WHEN rfm IN ('111', '112', '113', '114', '121', '122', '123') THEN 'Needs Attention'
+	WHEN rfm IN ('111', '112', '113', '114', '121', '122', '123') THEN 'Lost Customer'
 END AS segment
 FROM rfm_all
 )
 
 /* Count number of customers and revenue for each segment */
-SELECT segment, COUNT(customer_id) as num_cus, SUM(monetary) as rev
-FROM rfm_all
+SELECT segment, COUNT(customer_id) AS NUM
+FROM rfm_all_2
+GROUP BY segment
+ORDER BY NUM DESC
+
+-- SELECT COUNT (DISTINCT customer_id) FROM transactions
